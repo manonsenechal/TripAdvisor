6aced78c5468dd90414fe8ed19f658a41c52d62b
manonsenechal <manon.senechal@oclock.school>
2023-09-28 10:38:57 +0200

    sauvegarde du darkmode en localstorage

--- js/theme.js
+++ js/theme.js
@@ -7,6 +7,10 @@ const theme = {
 
         // ajouter l'écouteur d'évènement
         darkModeButton.addEventListener('click', theme.toggleDarkMode);
+
+        // on récupère le choix du darkMode dans le localStorage
+         // et on met à jour l'affichage
+         theme.loadDarkModeFromStorage();
     },
 
     toggleDarkMode: function() {
@@ -17,7 +21,32 @@ const theme = {
         // Avec toggle, cela permet directement d'ajouter la classe si elle n'est pas présente
         // et de la supprimer si elle déjà présente
         bodyElement.classList.toggle('theme-dark');
-    }
+
+        //ons auvegarde le choix de darkMode
+        theme.saveDarkModeInStorage();
+        },
+
+        loadDarkModeFromStorage: function(){
+
+            //on recup la valeur du darkmode depuis le local storage
+            const isdarkMode = Json.parse(localStorage.getItem('darkMode'));
+
+            //on passe en affichage darkmode si necessaire
+            if (isdarkMode){
+                document.body.classList.add('theme-dark');
+            }
+        },
+
+        saveDarkModeInStorage: function (){
+            // il fuat savoir si le darkmode est activé ou non 
+            let isdarkMode = false;
+            if (document.body.classList.contains('theme-dark')){
+                isdarkMode = true;
+            }
+
+             // on sauvegarde dans le localstorage
+             localStorage.setItem('darkMode', JSON.stringify(isdarkMode));
+        }
 };
 
 document.addEventListener('DOMContentLoaded', theme.init);
